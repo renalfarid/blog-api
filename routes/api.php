a9938603918df4b739d55c85c79586fa1fc22100
renalfarid <enal.farid@gmail.com>
2024-05-16 22:32:17 +0800

    remove unused route

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\UsersController;
 
 Route::get('/', function (){
    return response()->json(["message" => "welcome API"]);
 });
-/*Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');*/
+
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', [AuthController::class, 'auth'])->name('login');
     Route::post('signup', [AuthController::class, 'signup']);
