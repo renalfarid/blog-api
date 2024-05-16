594d009e66b4d117abb667f107da37bd1a3a04b9
renalfarid <enal.farid@gmail.com>
2024-05-17 01:31:20 +0800

    join post and users table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,18 @@ class Post extends Model
 
     public function scopeAllPost() 
     {
-       return Post::All();
+        $posts = Post::leftJoin('users', 'posts.author_id', '=', 'users.id')
+        ->select('posts.id', 'posts.title', 'users.name', 'posts.slug', 'posts.content', 'posts.published_date')
+        ->orderBy('posts.published_date', 'DESC')
+        ->get();
+       return $posts;
+    }
+
+    public function scopeById($query, $userId)
+    {
+      $posts = $query->where('author_id', $userId)
+               ->orderBy('published_date', 'DESC')
+                ->get();
+      return $posts;
     }
 }
